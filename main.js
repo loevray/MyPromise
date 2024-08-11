39ed5b224cb79c4027d65ce95243a2d493972516
loevray <rei980213@gmail.com>
2024-08-12 01:32:39 +0900

    Fix: test of my promise functions error fix

--- main.js
+++ main.js
@@ -3,54 +3,56 @@ import MyPromise from "./MyPromise.js";
 const testOfMyPromise = () =>
   new MyPromise((resolve) => {
     setTimeout(() => {
-      resolve();
-      console.log("promise 테스트 입니다1");
+      resolve("promise 테스트 입니다1");
     }, 3000);
   });
 
-const p1 = new MyPromise((resolve) =>
-  setTimeout(() => {
-    resolve("promise all test용 3초뒤 resolve");
-  }, 3000)
-);
+const p1 = () =>
+  new MyPromise((resolve) =>
+    setTimeout(() => {
+      resolve("promise all test용 3초뒤 resolve");
+    }, 3000)
+  );
 
-const p2 = new MyPromise((_, reject) =>
-  setTimeout(() => {
-    reject("promise all test용 5초뒤 reject");
-  }, 5000)
-);
+const p2 = () =>
+  new MyPromise((_, reject) =>
+    setTimeout(() => {
+      reject("promise all test용 5초뒤 reject");
+    }, 5000)
+  );
 
 const p3 = "hi";
 
-const testOfMyPromiseAll = () =>
-  MyPromise.all([p1, p2, p3])
-    .then((values) => console.log(values))
-    .catch((e) => console.log(`이행되지 않은 myPromise : ${e}`));
+const testOfMyPromiseAll = () => MyPromise.all([p1(), p2(), p3]);
 
 const p4 = () =>
   new MyPromise((resolve) =>
     setTimeout(() => {
-      resolve();
-      console.log("promise all race test용 2초뒤 resolve");
+      resolve("promise all race test용 2초뒤 resolve");
     }, 2000)
   );
 
 const p5 = () =>
   new MyPromise((resolve) =>
     setTimeout(() => {
-      resolve();
-      console.log("promise race test용 4초뒤 resolve");
+      resolve("promise race test용 4초뒤 resolve");
     }, 4000)
   );
 
-const testOfMyPromiseRace = () => MyPromise.race([p4, p5]);
+const testOfMyPromiseRace = () => MyPromise.race([p4(), p5()]);
 
 const testButtons = document.getElementById("testButtons");
 
 const myPromiseTestFunctions = [
-  testOfMyPromise,
-  testOfMyPromiseAll,
-  testOfMyPromiseRace,
+  () => testOfMyPromise().then((value) => console.log(value)),
+  () =>
+    testOfMyPromiseAll()
+      .then((values) => console.log(values))
+      .catch((e) => console.log(`이행되지 않은 myPromise : ${e}`)),
+  () =>
+    testOfMyPromiseRace()
+      .then((value) => console.log(value))
+      .catch((e) => console.log(`이행되지 않은 myPromise : ${e}`)),
 ];
 
 [...testButtons.children].forEach((testButton, i) => {
